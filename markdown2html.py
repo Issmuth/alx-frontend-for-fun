91f5c1f91a22c351fb74a41eab187815a189a714
issmuth <ismael.mdev@gmail.com>
2024-07-14 11:06:30 +0300

    done

---
+++ markdown2html.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Markdown to HTML"""
+import sys
+import os
+
+if len(sys.argv) < 3:
+    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+    exit(1)
+
+inputFileName = sys.argv[1]
+outputFileName = sys.argv[2]
+
+if not os.path.exists(inputFileName):
+    print("Missing {}".format(inputFileName), file=sys.stderr)
+    exit(1)
+else:
+    exit(0)
