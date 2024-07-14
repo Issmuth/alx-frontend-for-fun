83bfb0c441da524dba4d275fffe3193fce4d17d6
issmuth <ismael.mdev@gmail.com>
2024-07-14 11:11:06 +0300

    some fixes

--- markdown2html.py
+++ markdown2html.py
@@ -3,15 +3,16 @@
 import sys
 import os
 
-if len(sys.argv) < 3:
-    print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
-    exit(1)
-
-inputFileName = sys.argv[1]
-outputFileName = sys.argv[2]
-
-if not os.path.exists(inputFileName):
-    print("Missing {}".format(inputFileName), file=sys.stderr)
-    exit(1)
-else:
-    exit(0)
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
+    
+    inputFileName = sys.argv[1]
+    outputFileName = sys.argv[2]
+    
+    if not os.path.exists(inputFileName):
+        print("Missing {}".format(inputFileName), file=sys.stderr)
+        exit(1)
+    else:
+        exit(0)
