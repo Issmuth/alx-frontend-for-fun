5937bb3d9f4ee2bfe8b1e0dfd1346082949ab21f
issmuth <ismael.mdev@gmail.com>
2024-07-14 11:34:09 +0300

    done

--- markdown2html.py
+++ markdown2html.py
@@ -3,16 +3,37 @@
 import sys
 import os
 
+
+def headingParse(line):
+    """Parse markdown lines to appropriate HTML."""
+    if line.startswith("# "):
+        return("<h1>{}</h1>".format(line[2:]))
+    elif line.startswith("## "):
+        return("<h2>{}</h2>".format(line[3:]))
+    elif line.startswith("### "):
+        return("<h3>{}</h3>".format(line[4:]))
+    elif line.startswith("#### "):
+        return("<h4>{}</h4>".format(line[5:]))
+    elif line.startswith("##### "):
+        return("<h5>{}</h5>".format(line[6:]))
+    elif line.startswith("###### "):
+        return("<h6>{}</h6>".format(line[7:]))
+    else:
+        return line
+
 if __name__ == "__main__":
     if len(sys.argv) < 3:
         print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
         exit(1)
-    
+
     inputFileName = sys.argv[1]
     outputFileName = sys.argv[2]
-    
+
     if not os.path.exists(inputFileName):
         print("Missing {}".format(inputFileName), file=sys.stderr)
         exit(1)
     else:
-        exit(0)
+        with open(inputFileName, 'r') as file:
+            with open(outputFileName, 'a') as outFile:
+                for line in file:
+                    outFile.write(headingParse(line.strip())+"\n")
